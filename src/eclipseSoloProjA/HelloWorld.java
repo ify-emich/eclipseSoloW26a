da9171ccce624d38cc3a513e9d6a5c94b85db9b4
ify-emich <ieze@emich.edu>
2026-02-16 07:15:28 -0500

    Programmer 2 made more changes

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("A big big world");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer Two did this");
 	}
 }
